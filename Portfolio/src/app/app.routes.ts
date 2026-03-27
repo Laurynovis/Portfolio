1b6fb1a461175b8b6e929c208ca63768ff35ff77
Pedro <pelg.pedro@gmail.com>
2026-03-26 22:02:13 -0300

    Page-name update, again

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { Home } from './pages/home/home';
 import { ProjectDetail } from './pages/project-detail/project-detail';
 
 export const routes: Routes = [
-  { path: '', component: Home }, // Página inicial com sua bio
-  { path: 'projeto/:id', component: ProjectDetail }, // Rota dinâmica
+  { path: '', component: Home, title: 'Portfolio | Home' }, // Página inicial com sua bio
+  { path: 'projeto/:id', component: ProjectDetail, title: 'Portfolio | Project Detail' }, // Rota dinâmica
   { path: '**', redirectTo: '' } // Redireciona erros para a home
 ];
 
